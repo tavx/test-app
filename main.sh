6eaec4aa358287bda6cf28d5cbd9ed3e5b9393ba
Dmitry Lysenko <it@kankyo.ru>
2021-07-27 11:44:10 +0300

    fix git url and formatting

--- main.sh
+++ main.sh
@@ -4,12 +4,14 @@ echo "remove all html files"
 rm -rf ./*.html
 
 URL=${GIT_URL#"git@"}
+URL=${URL/://}
+URL=${URL%".git"}
 
 echo "print all env variables"
 cat << EOF > index.html
 <html>
 <h1>Hello from the simple Jenkins job </h1>
-<h2>git commit: https://${URL}/commit/${GIT_COMMIT}
+<h2>git commit: <a href="https://${URL}/commit/${GIT_COMMIT}">https://${URL}/commit/${GIT_COMMIT}</a></h2>
 <pre>
 $(printenv)
 </pre>
