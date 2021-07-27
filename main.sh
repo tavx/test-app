98932f142d47964f96daabefdd5ef08cfca3423a
Dmitry Lysenko <it@kankyo.ru>
2021-07-27 10:53:36 +0300

    html gen

--- main.sh
+++ main.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+echo "remove all html files"
+rm -rf ./*.html
+
 echo "print all env variables"
-printenv
+cat << EOF > index.html
+<html>
+<h1>Hello from simple Jenkins job</h2>
+<code>
+$(printenv)
+</code>
+</html>
+EOF
+
 touch test-variable
 #exit 1
