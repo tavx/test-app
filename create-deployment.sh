9cfc3c34e433a8413900eaafe5f5c23da79f2a97
Dmitry Lysenko <it@kankyo.ru>
2021-08-11 16:54:49 +0300

    index.html

---
+++ create-deployment.sh
@@ -0,0 +1,7 @@
+aws deploy create-deployment \
+  --application-name CodeDeployGitHubDemo-App \
+  --deployment-config-name CodeDeployDefault.OneAtATime \
+  --deployment-group-name CodeDeploy-Test-App-DepGroup \
+  --description "My GitHub deployment demo" \
+  --github-location repository=tavx/test-app,commitId=`git log origin | head -1 | cut -f 2 -d ' '`\
+  --ignore-application-stop-failures
